4de5ba9f653355a4f2825f3d891832274229ec0f
Koz Ross <koz.ross@retro-freedom.nz>
2021-10-29 18:41:35 +1300

    Manually unroll loop

--- src/count-eq.c
+++ src/count-eq.c
@@ -33,11 +33,31 @@ size_t diablo_count_eq(uint8_t const* const src,
   for (size_t i = 0; i < big_strides; i++) {
     uint64_t const* big_ptr = (uint64_t const*)ptr;
     uint64_t result = 0;
-    for (ptrdiff_t j = 0; j < 8; j++) {
-      uint64_t const input = (*(big_ptr + j)) ^ matches;
-      uint64_t const tmp = (input & mask) + mask;
-      result |= (~(tmp | input | mask) >> j);
-    }
+    // Manual 8x loop unroll
+    uint64_t input = *big_ptr ^ matches;
+    uint64_t tmp = (input & mask) + mask;
+    result |= ~(tmp | input | mask);
+    input = *(big_ptr + 1) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 1);
+    input = *(big_ptr + 2) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 2);
+    input = *(big_ptr + 3) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 3);
+    input = *(big_ptr + 4) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 4);
+    input = *(big_ptr + 5) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 5);
+    input = *(big_ptr + 6) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 6);
+    input = *(big_ptr + 7) ^ matches;
+    tmp = (input & mask) + mask;
+    result |= (~(tmp | input | mask) >> 7);
     count += __builtin_popcountll(result);
     ptr += 64;
   }
