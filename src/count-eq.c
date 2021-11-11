52bb52e933e397435ee388e841df32c7487e2e16
Koz Ross <koz.ross@retro-freedom.nz>
2021-11-11 17:00:46 +1300

    Some tidying

--- src/count-eq.c
+++ src/count-eq.c
@@ -21,49 +21,53 @@ static inline uint64_t broadcast(uint8_t const byte) {
   return byte * 0x0101010101010101ULL;
 }
 
+// Load a 64-bit word, then set every byte which matches to 0x80, while setting
+// the others to 0x00.
+static inline uint64_t load_and_set (uint64_t const * const big_ptr,
+                                     size_t const i,
+                                     uint64_t const matches,
+                                     uint64_t const mask) {
+  uint64_t const input = big_ptr[i] ^ matches;
+  uint64_t const tmp = (input & mask) + mask;
+  return (~(tmp | input | mask) >> i);
+}
+
+// Load and count a 64-byte block.
+static inline size_t count_block (uint8_t const * const src,
+                                  uint64_t const matches,
+                                  uint64_t const mask) {
+  uint64_t const* const big_ptr = (uint64_t const* const)src;
+  uint64_t result = 0;
+  // Manual 8x loop unroll
+  result |= load_and_set(big_ptr, 0, matches, mask);
+  result |= load_and_set(big_ptr, 1, matches, mask);
+  result |= load_and_set(big_ptr, 2, matches, mask);
+  result |= load_and_set(big_ptr, 3, matches, mask);
+  result |= load_and_set(big_ptr, 4, matches, mask);
+  result |= load_and_set(big_ptr, 5, matches, mask);
+  result |= load_and_set(big_ptr, 6, matches, mask);
+  result |= load_and_set(big_ptr, 7, matches, mask);
+  return __builtin_popcountll(result);
+}
+
 size_t diablo_count_eq(uint8_t const* const src,
                        size_t const off,
                        size_t const len,
                        uint8_t const byte) {
-  size_t count = 0;
-  // We step 64 bytes at a time.
+  size_t count = 0; 
+  // We aim to step 64 bytes at a time.
   size_t const big_strides = len / 64;
   size_t const small_strides = len % 64;
   uint8_t const* ptr = (uint8_t const*)&(src[off]);
-  // We use the method described in "Bit Twiddling Hacks".
-  // Source: https://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
-  uint64_t const matches = broadcast(byte);
-  uint64_t const mask = broadcast(0x7F);
-  for (size_t i = 0; i < big_strides; i++) {
-    uint64_t const* big_ptr = (uint64_t const*)ptr;
-    uint64_t result = 0;
-    // Manual 8x loop unroll
-    uint64_t input = *big_ptr ^ matches;
-    uint64_t tmp = (input & mask) + mask;
-    result |= ~(tmp | input | mask);
-    input = *(big_ptr + 1) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 1);
-    input = *(big_ptr + 2) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 2);
-    input = *(big_ptr + 3) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 3);
-    input = *(big_ptr + 4) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 4);
-    input = *(big_ptr + 5) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 5);
-    input = *(big_ptr + 6) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 6);
-    input = *(big_ptr + 7) ^ matches;
-    tmp = (input & mask) + mask;
-    result |= (~(tmp | input | mask) >> 7);
-    count += __builtin_popcountll(result);
-    ptr += 64;
+  if (big_strides != 0) {
+    // We use the method described in "Bit Twiddling Hacks".
+    // Source: https://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
+    uint64_t const matches = broadcast(byte);
+    uint64_t const mask = broadcast(0x7F);
+    for (size_t i = 0; i < big_strides; i++) {
+      count += count_block(ptr, matches, mask);
+      ptr += 64;
+    }
   }
   // Finish the rest slow.
   for (size_t i = 0; i < small_strides; i++) {
