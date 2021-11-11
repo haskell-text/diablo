24fd070c9fceb94a8320281f1ff8f1f20f35c1b1
Koz Ross <koz.ross@retro-freedom.nz>
2021-11-11 20:00:28 +1300

    Prepare for SIMDed solutions

--- src/count-eq.c
+++ src/count-eq.c
@@ -16,14 +16,46 @@
 #include <stddef.h>
 #include "../include/diablo.h"
 
+// Interface for all implementations.
+
+// Stores any implementation-specific data.
+typedef struct count_eq_env count_eq_env;
+
+// Build an environment based on what byte we're looking for.
+static inline count_eq_env setup_env (uint8_t const byte);
+
+// Retrieve the count.
+static inline size_t retrieve_count (count_eq_env const* env);
+
+// Load and count a 64-byte block.
+static inline void count_block (uint8_t const* const src,
+                                count_eq_env* env);
+
 // Fill every 8-byte 'lane' with the same value.
 static inline uint64_t broadcast(uint8_t const byte) {
   return byte * 0x0101010101010101ULL;
 }
 
+// Fallback implementation.
+
+struct count_eq_env {
+  uint64_t matches;
+  uint64_t mask;
+  size_t count;
+};
+
+static inline count_eq_env setup_env (uint8_t const byte) {
+  return (count_eq_env){ .matches = broadcast(byte), .mask = broadcast(0x7F), 
+                         .count = 0 };
+}
+
+static inline size_t retrieve_count (count_eq_env const* env) {
+  return env->count;
+}
+
 // Load a 64-bit word, then set every byte which matches to 0x80, while setting
 // the others to 0x00.
-static inline uint64_t load_and_set (uint64_t const * const big_ptr,
+static inline uint64_t load_and_set (uint64_t const* const big_ptr,
                                      size_t const i,
                                      uint64_t const matches,
                                      uint64_t const mask) {
@@ -33,23 +65,23 @@ static inline uint64_t load_and_set (uint64_t const * const big_ptr,
 }
 
 // Load and count a 64-byte block.
-static inline size_t count_block (uint8_t const * const src,
-                                  uint64_t const matches,
-                                  uint64_t const mask) {
+static inline void count_block (uint8_t const* const src,
+                                count_eq_env* env) {
   uint64_t const* const big_ptr = (uint64_t const* const)src;
   uint64_t result = 0;
   // Manual 8x loop unroll
-  result |= load_and_set(big_ptr, 0, matches, mask);
-  result |= load_and_set(big_ptr, 1, matches, mask);
-  result |= load_and_set(big_ptr, 2, matches, mask);
-  result |= load_and_set(big_ptr, 3, matches, mask);
-  result |= load_and_set(big_ptr, 4, matches, mask);
-  result |= load_and_set(big_ptr, 5, matches, mask);
-  result |= load_and_set(big_ptr, 6, matches, mask);
-  result |= load_and_set(big_ptr, 7, matches, mask);
-  return __builtin_popcountll(result);
+  result |= load_and_set(big_ptr, 0, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 1, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 2, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 3, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 4, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 5, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 6, env->matches, env->mask);
+  result |= load_and_set(big_ptr, 7, env->matches, env->mask);
+  env->count += __builtin_popcountll(result);
 }
 
+// Function implementation (fairly generic).
 size_t diablo_count_eq(uint8_t const* const src,
                        size_t const off,
                        size_t const len,
@@ -62,12 +94,12 @@ size_t diablo_count_eq(uint8_t const* const src,
   if (big_strides != 0) {
     // We use the method described in "Bit Twiddling Hacks".
     // Source: https://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
-    uint64_t const matches = broadcast(byte);
-    uint64_t const mask = broadcast(0x7F);
+    count_eq_env env = setup_env(byte);
     for (size_t i = 0; i < big_strides; i++) {
-      count += count_block(ptr, matches, mask);
+      count_block(ptr, &env);
       ptr += 64;
     }
+    count += retrieve_count(&env); 
   }
   // Finish the rest slow.
   for (size_t i = 0; i < small_strides; i++) {
